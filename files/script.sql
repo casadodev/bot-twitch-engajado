aba574fc0fb263dd26c721c67a2548ece5586c99
Dagmar Cirino <dagmar.mv@gmail.com>
2020-07-25 18:06:40 -0300

    continuando upload

---
+++ files/script.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS users (
+	UserID text PRIMARY KEY,
+	UserName text,
+	MessagesSent integer DEFAULT 0,
+	Coins integer DEFAULT 0,
+	CoinLock text DEFAULT CURRENT_TIMESTAMP,
+	Warnings integer DEFAULT 0,
+	BansCoint integer DEFAULT 0
+);
